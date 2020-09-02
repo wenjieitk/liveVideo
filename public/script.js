3126e2c96e8cda315818c5d461e4fa82745b3078
Wen Jie <wenjiens15@gmail.com>
2020-09-02 21:42:46 +0800

    remove key

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  key: '123123123',
   debug: 3
 })
 const myVideo = document.createElement('video')
